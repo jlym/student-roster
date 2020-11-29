b2bbbc7e5142c252167f7a8312197ccdf0d73dea
Jeffrey Lym <jeffreyalym@gmail.com>
2020-11-28 16:34:20 -0800

    Get working with tokio 0.2

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,10 @@
-use tokio_postgres::{Client, Config, Error, NoTls};
+use deadpool_postgres::config::Config;
+use deadpool_postgres::{ManagerConfig, Pool, RecyclingMethod};
+use tokio_postgres::{Error, NoTls};
 
+#[derive(Clone)]
 pub struct DB {
-    pub client: Client,
+    pub pool: Pool,
 }
 
 pub struct Student {
@@ -13,26 +16,20 @@ pub struct Student {
 impl DB {
     pub async fn new(host: &str, dbname: &str, user: &str, password: &str) -> Result<Self, Error> {
         let mut config = Config::new();
-        let config = config
-            .host(host)
-            .dbname(dbname)
-            .user(user)
-            .password(password);
-        let (client, connection) = config.connect(NoTls).await?;
+        config.host = Some(String::from(host));
+        config.dbname = Some(String::from(dbname));
+        config.user = Some(String::from(user));
+        config.password = Some(String::from(password));
 
-        tokio::spawn(async move {
-            if let Err(e) = connection.await {
-                eprintln!("connection: error: {}", e);
-            }
-        });
+        let pool = config.create_pool(NoTls).unwrap();
 
-        let db = Self { client };
+        let db = Self { pool };
         Ok(db)
     }
 
     pub async fn add_student(&self, name: &str, grade: i32) -> Result<Student, Error> {
-        let rows = self
-            .client
+        let client = self.pool.get().await.unwrap();
+        let rows = client
             .query(
                 "INSERT INTO students (name, grade)
                 VALUES($1, $2)
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod db;
 
+use actix_web::{get, web, App, HttpServer, Responder};
 use db::DB;
-use tokio_postgres::Error;
 
+/*
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let db = DB::new("localhost", "roster", "postgres", "password").await?;
@@ -13,4 +14,35 @@ async fn main() -> Result<(), Error> {
     println!("student name: {}", student.name);
     println!("student grade: {}", student.grade);
     Ok(())
+}
+*/
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let db = DB::new("localhost", "roster", "postgres", "password")
+        .await
+        .unwrap();
+    HttpServer::new(move || App::new().data(db.clone()).service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
+
+/*
+#[get("/")]
+async fn index(_: web::Data<DB>) -> String {
+    String::from("wow")
+}
+*/
+
+#[get("/")]
+async fn index(data: web::Data<DB>) -> String {
+    let grade: i32 = 2;
+    let student = data.add_student("Jeffrey", grade).await.unwrap();
+
+    println!("student id: {}", student.id);
+    println!("student name: {}", student.name);
+    println!("student grade: {}", student.grade);
+
+    student.name.clone()
 }
